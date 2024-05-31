8f71af137a96bb915ab6b66559bc565838c4f74a
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-05-31 14:44:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
-from vllm import LLM, SamplingParams
-from pathlib import Path
-from transformers import AutoTokenizer
+from transformers import T5ForConditionalGeneration, AutoTokenizer
+import torch
 
 class InferlessPythonModel:
     def initialize(self):
-        model_id = "rbgo/inferless-llama-3-8B"  # Specify the model repository ID
-        # Define sampling parameters for model generation
-        self.sampling_params = SamplingParams(temperature=0.7, top_p=0.95, max_tokens=128)
-        # Initialize the LLM object
-        self.llm = LLM(model=model_id)
+        model_id = "google/flan-ul2"  # Specify the model repository ID
+        self.model = T5ForConditionalGeneration.from_pretrained(model_id, device_map="auto", load_in_8bit=True)                                                                 
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
         
     def infer(self,inputs):
-        prompts = inputs["prompt"]  # Extract the prompt from the input
-        chat_format = [{"role": "user", "content": prompts}]
-        text = self.tokenizer.apply_chat_template(chat_format,tokenize=False,add_generation_prompt=True)
-        result = self.llm.generate(text, self.sampling_params)
-        # Extract the generated text from the result
-        result_output = [output.outputs[0].text for output in result]
-
-        # Return a dictionary containing the result
-        return {'generated_text': result_output[0]}
+        input_string = inputs["prompt"]
+        inputs = self.tokenizer(input_string, return_tensors="pt").input_ids.to("cuda")
+        outputs = self.model.generate(inputs, max_length=200)
+        result_output = self.tokenizer.decode(outputs[0],skip_special_tokens=True)
+        return {'generated_result': result_output}
 
     def finalize(self):
         pass
